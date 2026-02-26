8bc145a80632c079622bbe97ef5b23fac3e00d78
Estifanos16 <abrhamestifanos17@gmail.com>
2026-02-26 10:09:44 -0800

    first upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+
+// Load env variables
+dotenv.config();
+
+// Connect database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
